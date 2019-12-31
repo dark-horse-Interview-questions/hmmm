71c782f422d6e5e84af13741db71f8a614482e71
Your Name <you@example.com>
2019-12-31 17:33:44 +0800

    初始化项目

--- src/module-hmmm/pages/questions.vue
+++ src/module-hmmm/pages/questions.vue
@@ -1,6 +1,23 @@
 <template>
   <div class="question-container">
-    基础题库
+    <div class="el-card">
+      <div class='el-card__header'>
+        <el-row>
+          <el-button type="primary">新增试题</el-button>
+          <el-button type="primary">批量导入</el-button>
+        </el-row>
+      </div>
+      <div class="el-card_body">
+         <div class="el-form-item el-form-item--medium">
+           <el-form-item label="学科">
+              <!-- <el-select v-model="sizeForm.region" placeholder="请选择"> -->
+            </el-form-item>
+         </div>
+      </div>
+    </div>
+
+    
+   
   </div>
 </template>
 
@@ -8,4 +25,12 @@
 
 </script>
 
-<style lang="scss"></style>
+<style lang="scss">
+  // .el-card_header{
+  //   padding: 18px 20px;
+  //   border-bottom: 1px solid #ebeef5;
+  //   -webkit-box-sizing: border-box;
+  //   box-sizing: border-box;
+  // }
+
+</style>
