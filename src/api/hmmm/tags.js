1ead94deb61944c76c6647e4538eec4549148f4d
unknown <942165499@qq.com>
2020-01-05 10:32:35 +0800

    hhhgcc

--- src/api/hmmm/tags.js
+++ src/api/hmmm/tags.js
@@ -10,24 +10,24 @@
 import { createAPI } from '@/utils/request'
 
 // 标签列表
-export const list = data => createAPI('/tags', 'get', data)//完成
+export const list = data => createAPI('/tags', 'get', data)
 
 // 标签简单列表
-export const simple = data => createAPI('/tags/simple', 'get', data)//完成
+export const simple = data => createAPI('/tags/simple', 'get', data)
 
 // 标签详情
-export const detail = data => createAPI(`/tags/${data.id}`, 'get', data)//完成
+export const detail = data => createAPI(`/tags/${data.id}`, 'get', data)
 
 // 标签添加
-export const add = data => createAPI(`/tags`, 'post', data)//完成
+export const add = data => createAPI(`/tags`, 'post', data)
 
 // 标签修改
-export const update = data => createAPI(`/tags/${data.id}`, 'put', data)//完成
+export const update = data => createAPI(`/tags/${data.id}`, 'put', data)
 
 // 标签删除
-export const remove = data => createAPI(`/tags/${data.id}`, 'delete', data)//完成
+export const remove = data => createAPI(`/tags/${data.id}`, 'delete', data)
 
 // 标签状态
-export const removeState = data => createAPI(`/tags/${data.id}/${data.state}`, 'post', data)//完成
+export const removeState = data => createAPI(`/tags/${data.id}/${data.state}`, 'post', data)
 
 // removeState({id,state})   如果参数对了   刷新页面没有更新   接口问题。
--- src/module-hmmm/pages/directorys.vue
+++ src/module-hmmm/pages/directorys.vue
@@ -1,216 +1,218 @@
 <template>
+  <!-- 放置一个容器 -->
   <el-card>
-    <!-- 新建目录区域 -->
-      <el-row slot="header">
-        <el-button type="primary" @click="dialogVisible = true">新建目录</el-button>
-        <el-button type="primary">返回学科</el-button>
-      </el-row>
-      <!-- 搜索区域form区 -->
-      <el-form inline>
-        <el-form-item label="目录名称" >
-          <el-input v-model="searchForm.directoryName" ></el-input>
+    <el-row slot="header">
+      <el-button type="primary" @click="dialogVisible = true">新建目录</el-button>
+      <el-button type="primary">返回学科</el-button>
+    </el-row>
+    <!-- 表单容器 -->
+    <el-form inline>
+      <el-form-item label="目录名称">
+        <el-input v-model="searchForm.directoryName"></el-input>
+      </el-form-item>
+      <el-form-item label="状态">
+        <el-select v-model="searchForm.state">
+          <el-option
+            v-for="item in status"
+            :key="item.value"
+            :value="item.value"
+            :label="item.label"
+          ></el-option>
+        </el-select>
+      </el-form-item>
+      <el-form-item>
+        <el-button @click="clear">清除</el-button>
+        <el-button @click="search" type="primary">搜索</el-button>
+      </el-form-item>
+    </el-form>
+    <!-- 表格 -->
+    <el-table :data="list">
+      <el-table-column prop="id" label="序号"></el-table-column>
+      <el-table-column prop="directoryName" label="目录名称"></el-table-column>
+      <el-table-column prop="creator" label="创建者"></el-table-column>
+      <!-- 属性 formatter -->
+      <el-table-column prop="addDate" label="创建日期">
+        <!-- 作用域插槽 -->
+        <!-- 过滤器  表达式 | 过滤器名称 -->
+        <template slot-scope="obj">{{ obj.row.addDate | parseTimeByString}}</template>
+      </el-table-column>
+      <el-table-column prop="totals" align="center" label="面试题数量"></el-table-column>
+      <el-table-column prop="state" align="center" label="状态">
+        <template slot-scope="obj">{{ obj.row.state === 1 ? '开启' : '关闭 ' }}</template>
+      </el-table-column>
+      <el-table-column label="操作">
+        <template slot-scope="obj">
+          <el-button @click="modify(obj.row.id)" type="text">修改</el-button>
+          <el-button
+            @click="changeState(obj.row)"
+            type="text"
+          >{{ obj.row.state === 1 ? '禁用' : '开启' }}</el-button>
+          <el-button @click="delItem(obj.row.id)" type="text">删除</el-button>
+        </template>
+      </el-table-column>
+    </el-table>
+    <!-- 分页 -->
+    <el-row type="flex" justify="end" style="margin:30px 0">
+      <el-pagination
+        background
+        layout="prev,pager,next"
+        :total="page.total"
+        :current-page="page.currentPage"
+        :page-size="page.pageSize"
+        @current-change="changePage"
+      ></el-pagination>
+    </el-row>
+    <el-dialog :show-close="false" :visible="dialogVisible">
+      <!-- 主体内容 -->
+      <el-form ref="myForm" :model="formData" :rules="rules" label-width="80px">
+        <el-form-item label="目录名称" prop="directoryName">
+          <el-input v-model="formData.directoryName" style="width:50%"></el-input>
         </el-form-item>
-        <el-form-item label="状态">
-         <el-select v-model="searchForm.state">
-           <el-option v-for="item in status" :key="item.value" :value="item.value" :label="item.label">
-
-           </el-option>
-         </el-select>
-        </el-form-item>
-        <el-form-item >
-           <el-button @click="clear">清除</el-button>
-          <el-button @click="search" type="primary">搜索</el-button>
+        <el-form-item label="学科" prop="subjectID">
+          <el-select v-model="formData.subjectID" style="width:50%">
+            <el-option
+              v-for="item in subjects"
+              :key="item.value"
+              :label="item.label"
+              :value="item.value"
+            ></el-option>
+          </el-select>
         </el-form-item>
       </el-form>
-      <!-- el-table表格区域 -->
-      <el-table :data="getlist">
-        <el-table-column prop="id" label="序号"></el-table-column>
-        <el-table-column prop="directoryName" label="目录名称"></el-table-column>
-        <el-table-column prop="creator" label="创建者"></el-table-column>
-        <el-table-column prop="addDate" label="创建时间">
-          <!-- 改变事件格式 -->
-          <!-- 用作用域插槽或者formatter这里面用作用域插槽 -->
-          <!--slot-scope="obj" 可以获取当前行的数据 -->
-          <template slot-scope="obj">
-            <!-- 这里用提前封装好的过滤器改变事件格式 -->
-            {{obj.row.addDate | parseTimeByString}}
-          </template>
-        </el-table-column>
-        <el-table-column prop="totals" label="面试题数量" align="center"></el-table-column>
-        <el-table-column prop="state" label="状态" align="center">
-          <template slot-scope="obj">
-            {{obj.row.state===1?'启用':'禁用'}}
-          </template>
-
-        </el-table-column>
-        <el-table-column label="操作">
-          <template slot-scope="obj">
-            <el-button type="text">修改</el-button>
-            <el-button type="text" @click="changeState(obj.row)">{{obj.row.state===1?'禁用':'启用'}}</el-button>
-            <el-button type="text" @click="delItem(obj.row.id)">删除</el-button>
-          </template>
-          
-        </el-table-column>
-      </el-table>
-       <!-- 分页区域 -->
-      <el-row type="flex" justify="end" style="margin:30px 0">
-         <el-pagination
-          background
-          layout="prev,pager,next"
-          :total="page.total"
-          :page-size="page.pageSzie"
-          :current-page="page.currentPage"
-      >
-      </el-pagination>
+      <el-row slot="footer" type="flex" justify="end">
+        <el-button type="primary" @click="submitForm">确定</el-button>
+        <el-button @click="btnCancel">取消</el-button>
       </el-row>
-      <!-- 弹层 -->
-      <el-dialog :show-close="false" :visible="dialogVisible">
-        <!-- 弹层主题 -->
-        <el-form label-width="80px" :model="formData" :rules="rules" ref="myForm">
-          <el-form-item prop="directoryName" label="目录名称">
-            <el-input style="width:50%" v-model="formData.directoryName"></el-input>
-          </el-form-item>
-          <el-form-item prop="subjectID"  label="学科">
-            <el-select style="width:50%" v-model="formData.subjectID">
-              <el-option v-for="item in Subjects" :key="item.value" :value="item.value" :label="item.label"></el-option>
-            </el-select>
-          </el-form-item>
-          
-        </el-form>
-          <el-row slot="footer" type="flex" justify="end">
-            <el-button type="primary" @click="submitForm">确定</el-button>
-            <el-button> 取消</el-button>
-          </el-row>
-      </el-dialog>
+    </el-dialog>
   </el-card>
- 
-  
 </template>
 
 <script>
-import {list as getList,
-        remove as removeDirectory,
-        removeState as changeState,
-        simple as getSubjects,
-        add as addDirectory} from '../../api/hmmm/directorys'
-import {status} from '../../api/hmmm/constants'
+import {
+  list as DirectorysList,
+  remove as removeDirectory,
+  removeState as changeState,
+  add as addDirectory,
+  detail as detailDirectory,
+  update as updateDirectory
+} from "../../api/hmmm/directorys";
+import { status } from "../../api/hmmm/constants";
+import { simple as getSubjects } from "../../api/hmmm/subjects";
 export default {
-  name: 'DirectorysList',
+  name: "DirectorysList",
   data() {
     return {
-      //弹层
-      dialogVisible:false,
-      
-      // 定义目录名称和状态的数据
-      //并且V-model绑定
-      searchForm:{
-        directoryName:"",
-        state:""
-
+      dialogVisible: false, // 控制弹框的显示和隐藏
+      // 搜索对象
+      searchForm: {
+        directoryName: "", // 目录名称
+        state: null // 默认没状态
       },
-      //定义列表数据是个空数组
-      getlist:[],
-    //定义分页对象
-      page:{
-        pageSize:10,
-        currentPage:1,
-        total:0
+      list: [], // 列表的数据
+      page: {
+        currentPage: 1, // 当前页码
+        pageSize: 10, // 每页条数
+        total: 0 // 总数量
       },
-      status,
-      //弹层数据定义
-      formData:{
-        subjectID:'',
-        directoryName:'',
+      status, // 状态数据
+      formData: {
+        subjectID: null,
+        directoryName: ""
       },
-      rules:{
-        subjectID:[{required:true,message:'内容不能为空'}],
-        directoryName:[{required:true,message:'内容不能为空'}]
+      rules: {
+        subjectID: [
+          { required: true, message: "学科id不能为空", trigger: "blur" }
+        ],
+        directoryName: [
+          { required: true, message: "目录名称不能为空", trigger: "blur" }
+        ]
       },
-      Subjects:[]
+      subjects: [] // 用来接收数据
+    };
+  },
+  methods: {
+    changePage(newPage) {
+      this.page.currentPage = newPage;
+      this.getDirectory(this.searchForm);
+    },
+    // 两种方案 1 直接修改前端数据(行不通，因为前端数据没有subjectID) 2  从后端查数据 给前端 再修改
+    async modify(id) {
+      // // 方案1
+      // this.formData = row; // 直接获取我的数据
+      // this.dialogVisible = true;
+      let result = await detailDirectory({ id });
+      this.formData = result.data;
+      this.dialogVisible = true;
+    },
+    // 校验表单 并且提交
+    async submitForm() {
+      // 支持两种模式  回调函数  promise
+      await this.$refs.myForm.validate();
+      // 调用新增接口
+      this.formData.id
+        ? await updateDirectory(this.formData) // 是由id的
+        : await addDirectory(this.formData); // id存在调用修改接口 否则调用新增接口
+      //  操作成功
+      this.$message({ type: "success", message: "操作成功" });
+      // 重置数据
+      this.getDirectory(this.searchForm); // 应该带状态查询
+      this.btnCancel();
+    },
+    btnCancel() {
+      this.formData = {
+        subjectID: null,
+        directoryName: ""
+      };
+      this.dialogVisible = false; // 关闭弹层
+    },
+    // 切换状态
+    async changeState(row) {
+      await this.$confirm("您是否确定改变此状态?");
+      await changeState({ id: row.id, state: row.state === 1 ? 0 : 1 }); // 状态是反着的
+      this.$message({ type: "success", message: "改变状态成功" });
+      this.getDirectory(this.searchForm); // 应该带状态查询
+    },
+    // 删除方法
+    async delItem(id) {
+      await this.$confirm("您是否确定删除此数据?");
+      // 确定删除
+      await removeDirectory({ id });
+      this.$message({ type: "success", message: "删除成功" });
+      this.getDirectory(this.searchForm); // 应该带状态查询
+    },
+    search() {
+      // 搜索方法
+      this.page.currentPage = 1; // 搜索回到第一页
+      this.getDirectory(this.searchForm);
+    },
+    // 将数据还原为初始状态
+    clear() {
+      this.searchForm = {
+        directoryName: "", // 目录名称
+        state: null // 默认没状态
+      };
+    },
+    // 获取目录数据
+    async getDirectory(data) {
+      // 调用接口
+      let result = await DirectorysList({
+        page: this.page.currentPage,
+        pagesize: this.page.pageSize,
+        ...data
+      });
+      this.list = result.data.items; // 列表数据
+      this.page.total = result.data.counts; // 总条数
+    },
+    async getSubjects() {
+      let result = await getSubjects();
+      this.subjects = result.data;
     }
   },
-  methods:{
-    //手动校验表单
-    async submitForm(){
-    await this.$refs.myForm.validate();
-    //调用新增接口add
-    await addDirectory(this.formData);
-    //成功提示
-    this.$message({
-      type:'success',
-      message:'提交成功'
-    });
-    //清空弹层
-    this.formData ={
-      subjectID:'',
-      directoryName:'',
-    };
-    //关闭弹层
-    this.dialogVisible = false;
-
-    },
-
-    //切换状态
-   async changeState(row){
-     await this.$confirm('您确定改变此状态')
-     await changeState({id:row.id,state: row.state === 1 ? 0 : 1})
-     this.$message({type:"success",message:'改变状态成功'})
-       this.getDirectory(this.searchForm )
-    },
-    //获取弹层下拉菜单
-   async getSubjects(){
-     let result =await getSubjects()
-     this.Subjects = result.data
-
-    },
-    //删除功能
-   async delItem (id) {
-        //友好提示
-    await this.$confirm('您确定要删除吗')
-    await  removeDirectory({id})
-    this.$message({
-      type:'success',
-      message:'删除成功'
-    })
-    //带状态查询所以要携带参数    
-   this.getDirectory(this.searchForm )
-      
-    },
-    //搜索功能
-    search(){
-      this.page.currentPage=1,
-      this.getDirectory(this.searchForm)
-
-    },
-    //清除数据
-      clear(){
-        this.searchForm = {
-        directoryName:"",
-        state:""
-
-      }
-      },
-    //加载数据
-    async getDirectory(data){
-      //通过接口得知getList需要传递参数
-      //page/pagesize
-    let result = await getList({
-      page:this.currentPage,
-      pagesize:this.pageSize,
-      ...data
-    })
-    this.getlist = result.data.items//列表数据
-    this.page.total = result.data.count//总条数
-    }
-    
-    },
-   created(){
-     this.getSubjects()//调用学科
-     this.getDirectory()
-   } 
-
-  
-}
+  created() {
+    this.getSubjects(); // 调用学科
+    this.getDirectory();
+  }
+};
 </script>
 
 <style scoped>
